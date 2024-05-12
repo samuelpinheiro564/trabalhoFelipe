7d88f938e516789644311c977d7e2ae536d8e50d
andreybotero <125027722+andreybotero@users.noreply.github.com>
2024-05-12 20:29:31 -0300

    feat: implement registration form in CadastrarEmps screen

--- src/screens/CadastrarEmps/index.jsx
+++ src/screens/CadastrarEmps/index.jsx
@@ -1,38 +1,44 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import apiRequests from '../../services/api';
-import { useNavigation } from '@react-navigation/native';
-import Login from '../Login';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import apiRequests from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
+import Login from "../Login";
 
 export default function CadastrarEmps() {
-  const [nome, setNome] = useState('');
-  const [cnpj, setCnpj] = useState('');
-  const [cep,setCep] = useState('');
-  const [email, setEmail] = useState('');
-  const [area_atuacao,setArea_atuacao] = useState('');
-  const [senha, setSenha] = useState('');
+  const [nome, setNome] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [cep, setCep] = useState("");
+  const [email, setEmail] = useState("");
+  const [area_atuacao, setArea_atuacao] = useState("");
+  const [senha, setSenha] = useState("");
   const [msgErro, setMsgErro] = useState("");
   const [erro, setErro] = useState(false);
 
   const navigation = useNavigation();
 
   const Cadastro = async () => {
-    const dadosEmpresa = {cnpj, nome, cep, area_atuacao, email, senha };
+    const dadosEmpresa = { cnpj, nome, cep, area_atuacao, email, senha };
     try {
-      if(!nome || !cnpj || !cep || !email || !area_atuacao || !senha){
+      if (!nome || !cnpj || !cep || !email || !area_atuacao || !senha) {
         setErro(true);
         setMsgErro("Preencha todos os campos!");
         return;
-      }else{
-      await apiRequests.CadastrarEmpresa(dadosEmpresa);
-      console.log("nome cadastrada com sucesso!");
-      navigation.navigate(Login);
-    }
+      } else {
+        await apiRequests.CadastrarEmpresa(dadosEmpresa);
+        console.log("nome cadastrada com sucesso!");
+        navigation.navigate(Login);
+      }
     } catch (error) {
       console.error(error.message);
-    } 
-};
+    }
+  };
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Cadastro</Text>
@@ -84,8 +90,8 @@ export default function CadastrarEmps() {
       <View style={styles.line} />
       <TouchableOpacity style={styles.button} onPress={Cadastro}>
         <Text style={styles.buttonText}>finalizar Cadastro.</Text>
-      </TouchableOpacity >
-      
+      </TouchableOpacity>
+
       {erro && <Text style={styles.errorMessage}>{msgErro}</Text>}
     </ScrollView>
   );
@@ -94,52 +100,51 @@ export default function CadastrarEmps() {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     marginBottom: 20,
     padding: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
   },
   button: {
-    backgroundColor: '#008000',
+    backgroundColor: "#008000",
     padding: 10,
     borderRadius: 5,
     marginBottom: 20,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
   },
   registerText: {
     marginTop: 20,
   },
   registerLink: {
-    color: '#008000',
-    fontWeight: 'bold',
+    color: "#008000",
+    fontWeight: "bold",
   },
   line: {
     height: 1,
-    backgroundColor: 'black',
-    width: '100%',
+    backgroundColor: "black",
+    width: "100%",
     marginBottom: 20,
   },
   errorMessage: {
-    color: 'red',
-    textAlign: 'center',
+    color: "red",
+    textAlign: "center",
   },
 });
-  
